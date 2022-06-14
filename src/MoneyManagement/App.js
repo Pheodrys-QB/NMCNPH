e217c1f209b375f81b66ea0cb72083994623a373
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2022-06-14 19:03:11 +0700

    Update App.js

--- src/MoneyManagement/App.js
+++ src/MoneyManagement/App.js
@@ -1,63 +1,65 @@
-import React, {useState} from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import React from 'react';
+
 import TransactionScreen from './screens/TransactionScreen';
 import AddScreen from './screens/AddScreen';
 
-
 const App = () => {
-
   return (
     //<TransactionScreen />
     <AddScreen />
   );
-      { <KeyboardAvoidingView 
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-        <TextInput style={styles.input} placeholder={'Write a task'} value={trans} onChangeText={text => setTrans(text)} />
-        <TouchableOpacity onPress={() => handleAddTrans()}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
-          </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
 
-     }
+  // {
+  //   <KeyboardAvoidingView
+  //     behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+  //     style={styles.writeTaskWrapper}>
+  //     <TextInput
+  //       style={styles.input}
+  //       placeholder={'Write a task'}
+  //       value={trans}
+  //       onChangeText={text => setTrans(text)}
+  //     />
+  //     <TouchableOpacity onPress={() => handleAddTrans()}>
+  //       <View style={styles.addWrapper}>
+  //         <Text style={styles.addText}>+</Text>
+  //       </View>
+  //     </TouchableOpacity>
+  //   </KeyboardAvoidingView>;
+  // }
 
-  //);
 };
 
 export default App;
 
-const styles = StyleSheet.create({
-  writeTaskWrapper: {
-    position: 'absolute',
-    bottom: 60,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    width: 250,
-  },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-  },
-  addText: {},
-});
+// const styles = StyleSheet.create({
+//   writeTaskWrapper: {
+//     position: 'absolute',
+//     bottom: 60,
+//     width: '100%',
+//     flexDirection: 'row',
+//     justifyContent: 'space-around',
+//     alignItems: 'center',
+//   },
+//   input: {
+//     paddingVertical: 15,
+//     paddingHorizontal: 15,
+//     backgroundColor: '#FFF',
+//     borderRadius: 60,
+//     borderColor: '#C0C0C0',
+//     borderWidth: 1,
+//     width: 250,
+//   },
+//   addWrapper: {
+//     width: 60,
+//     height: 60,
+//     backgroundColor: '#FFF',
+//     borderRadius: 60,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     borderColor: '#C0C0C0',
+//     borderWidth: 1,
+//   },
+//   addText: {},
+// });
 
 //hello
