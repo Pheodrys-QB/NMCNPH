26e23d77eab621d41fc1f5b3a71de03e7a48dd3d
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2022-07-20 20:30:30 +0700

    quick change to auth

--- src/MoneyManagement/navigation/Routes.js
+++ src/MoneyManagement/navigation/Routes.js
@@ -2,15 +2,10 @@ import React, { useContext } from "react";
 import { AuthContext } from "./AuthProvider";
 import AppStack from "./AppStack"
 import AuthStack from "./AuthStack"
-import { onAuthStateChanged } from "firebase/auth"
-import { auth } from "../firebase-config"
 
 const Routes = () => {
-  const {user, setUser} = useContext(AuthContext);
+  const user = useContext(AuthContext);
   
-  onAuthStateChanged(auth, (currentUser)=>{
-    setUser(currentUser)
-  })
 
   return <>{user ? <AppStack /> : <AuthStack />}</>
 }
