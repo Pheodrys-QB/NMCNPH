53a41047f5cc6a6d9bdb0f8b1fef5ab10d7406d1
mingnhat <81601321+mingnhat@users.noreply.github.com>
2022-08-06 10:02:17 +0700

    Update MoreScreen.js

--- src/MoneyManagement/screens/MoreScreen.js
+++ src/MoneyManagement/screens/MoreScreen.js
@@ -2,24 +2,11 @@ import React, {useState, useEffect} from 'react';
 import {View, Button, Text} from 'react-native';
 import {auth} from '../firebase-config';
 import {signOut} from 'firebase/auth';
-
+import MoreTab from '../components/MoreTab';
 const MoreScreen = () => {
-  const [date, setDate] = useState(null)
-
-  const LogOut = async () => {
-    await signOut(auth);
-  };
-
-  useEffect(()=>{
-    const t = new Date('2022-08-04')
-    console.log(t)
-  }, [])
-
+  
   return (
-    <View>
-      <Button title="LogOut" onPress={LogOut} />
-      <Text> {date} </Text>
-    </View>
+    <MoreTab/>
     
   );
 };
